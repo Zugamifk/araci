d334f4a5ced3ed459aa8cb343232a4d5ff0ad7ac
zugamifk <zugamifk@gmail.com>
2022-11-15 23:50:19 -0400

    turn to face direction you're moving

--- Assets/Scripts/View/Character/Character.cs
+++ Assets/Scripts/View/Character/Character.cs
@@ -6,6 +6,9 @@ using UnityEngine;
 [RequireComponent(typeof(Identifiable))]
 public class Character : MonoBehaviour, IModelView<ICharacterModel>
 {
+    [SerializeField]
+    Transform _viewRoot;
+
     public Guid Id => _identifiable.Id;
     Identifiable _identifiable;
 
@@ -32,7 +35,13 @@ public class Character : MonoBehaviour, IModelView<ICharacterModel>
     void DoDesiredMove()
     {
         var movement = Game.Model.Movement.GetItem(_identifiable.Id);
+        var lastPosition = transform.position;
         Map.Instance.MoveObject(movement, transform);
+        var newPosition = transform.position;
+        var side = (newPosition - lastPosition).x;
+        var angle = side < 0 ? 180 : 0;
+        _viewRoot.transform.localRotation = Quaternion.Euler(0, angle, 0);
+
         Game.Do(new UpdatePosition(_identifiable.Id, transform.position));
     }
 }
