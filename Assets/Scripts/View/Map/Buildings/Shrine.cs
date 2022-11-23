6c3685933b189dc282334ad95663ead0cee38598
zugamifk <zugamifk@gmail.com>
2022-11-23 18:25:25 -0400

    add shrine

---
+++ Assets/Scripts/View/Map/Buildings/Shrine.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shrine : MonoBehaviour
+{
+    [SerializeField]
+    GameObject[] _candles;
+
+    private void Update()
+    {
+        
+    }
+}
