6b3b96c878fc3f82cecc3424a1afcaa9a2733a29
zugamifk <zugamifk@gmail.com>
2022-12-25 02:57:49 -0400

    cook and heat rates

--- Assets/Scripts/Services/Food/FoodService.cs
+++ Assets/Scripts/Services/Food/FoodService.cs
@@ -1,3 +1,4 @@
+using Codice.Client.Common;
 using Core;
 using System;
 using System.Collections;
@@ -8,8 +9,66 @@ namespace Food
 {
     public class FoodService
     {
+        public void ValidateFood(IFood food)
+        {
+            if (food.Weight <= 0)
+            {
+                throw new ArgumentException($"Food has weight of zero and therefore has nothing left!");
+            }
+
+            if (food.Volume <= 0)
+            {
+                throw new ArgumentException($"Food has volume of zero and therefore has nothing left!");
+            }
+
+            if (food.HeatTransferRate < 0)
+            {
+                throw new ArgumentException($"Heat transfer rate is {food.HeatTransferRate}! Can not be negative.");
+            }
+
+            if (food.SolidPercent < 0 || food.SolidPercent > 1)
+            {
+                throw new ArgumentException($"Heat SolidPercent is {food.SolidPercent}! Must be between 0 and 1.");
+            }
+
+            if (food.CookedPercent < 0 || food.CookedPercent > 1)
+            {
+                throw new ArgumentException($"Heat SolidPercent is {food.SolidPercent}! Must be between 0 and 1.");
+            }
+
+            if (food.CookRate < 0)
+            {
+                throw new ArgumentException($"Cook rate is {food.CookRate}! Can not be negative.");
+            }
+        }
+
         public void Heat(IFood food, float externalTemperature, float time)
         {
+            ValidateFood(food);
+
+            UpdateTemperature(food, externalTemperature, time);
+
+            if (food.Temperature > food.CookTemperature)
+            {
+                UpdateCookedPercent(food, time);
+            }
+        }
+
+        void UpdateTemperature(IFood food, float externalTemperature, float time)
+        {
+            var density = food.Weight / food.Volume;
+            var heatConductivity = (float)food.HeatTransferRate / density;
+            var heatDifferential = (externalTemperature - food.Temperature);
+
+            food.Temperature += time * heatDifferential * heatConductivity / density;
+        }
+
+        void UpdateCookedPercent(IFood food, float time)
+        {
+            var tempDifferential = food.Temperature - food.CookTemperature;
+            var cookedAmount = food.Volume * food.CookedPercent;
+            cookedAmount += time * tempDifferential * food.CookRate / (1 + food.SolidPercent);
+            food.CookedPercent = Mathf.Clamp01(cookedAmount / food.Volume);
         }
     }
 }
--- Assets/Scripts/Tests/Food/FoodService.cs
+++ Assets/Scripts/Tests/Food/FoodService.cs
@@ -10,10 +10,89 @@ namespace Food.Tests
 {
     public class FoodServiceTests
     {
-        const string TEST_FOOD_NAME = "Test Food";
+        FoodService _foodService = new();
 
-        FoodService _foodService;
+        #region ValidateFood
+        [Test]
+        public void ValidateFood_ZeroWeight_ThrowsArgumentException()
+        {
+            var food = new IFood_Mock();
+            food.Weight = 0;
+            Assert.Throws<ArgumentException>(() => _foodService.ValidateFood(food));
+        }
 
-        
+        [Test]
+        public void ValidateFood_ZeroVolume_ThrowsArgumentException()
+        {
+            var food = new IFood_Mock();
+            food.Volume = 0;
+            Assert.Throws<ArgumentException>(() => _foodService.ValidateFood(food));
+        }
+
+        [Test]
+        public void ValidateFood_HeatTransferRateNegative_ThrowsArgumentException()
+        {
+            var food = new IFood_Mock();
+            food.HeatTransferRate = -1;
+            Assert.Throws<ArgumentException>(() => _foodService.ValidateFood(food));
+        }
+
+        [Test]
+        public void ValidateFood_SolidPercentNegative_ThrowsArgumentException()
+        {
+            var food = new IFood_Mock();
+            food.SolidPercent = -1;
+            Assert.Throws<ArgumentException>(() => _foodService.ValidateFood(food));
+        }
+
+        [Test]
+        public void ValidateFood_SolidPercentAboveOne_ThrowsArgumentException()
+        {
+            var food = new IFood_Mock();
+            food.SolidPercent = 2;
+            Assert.Throws<ArgumentException>(() => _foodService.ValidateFood(food));
+        }
+
+        [Test]
+        public void ValidateFood_CookedPercentNegative_ThrowsArgumentException()
+        {
+            var food = new IFood_Mock();
+            food.CookedPercent = -1;
+            Assert.Throws<ArgumentException>(() => _foodService.ValidateFood(food));
+        }
+
+        [Test]
+        public void ValidateFood_CookedPercentAboveOne_ThrowsArgumentException()
+        {
+            var food = new IFood_Mock();
+            food.CookedPercent = 2;
+            Assert.Throws<ArgumentException>(() => _foodService.ValidateFood(food));
+        }
+
+        [Test]
+        public void ValidateFood_CookRateNegative_ThrowsArgumentException()
+        {
+            var food = new IFood_Mock();
+            food.CookRate = -1;
+            Assert.Throws<ArgumentException>(() => _foodService.ValidateFood(food));
+        }
+        #endregion
+
+        #region Heat
+        [Test]
+        [TestCase(1, 1, 1, 100, 100, 100)]
+        public void Heat_TestTemperatureChange(float weight, float volume, float heatTransferRate, float foodTemp, float externalTemp, float expectedTemp)
+        {
+            var food = new IFood_Mock();
+            food.Weight = weight;
+            food.Volume = volume;
+            food.HeatTransferRate = heatTransferRate;
+            food.Temperature = foodTemp;
+
+            _foodService.Heat(food, externalTemp, 1);
+
+            Assert.AreEqual(expectedTemp, food.Temperature);
+        }
+        #endregion
     }
 }
