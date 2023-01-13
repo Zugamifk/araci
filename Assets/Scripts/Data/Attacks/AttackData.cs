2b8b4ac3eec1fb14c80b3a26f6ba176daa73a76b
zugamifk <zugamifk@gmail.com>
2023-01-12 20:32:56 -0400

    reorganizing files

---
+++ Assets/Scripts/Data/Attacks/AttackData.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackData : ScriptableObject
+{
+    public float WindUpTime;
+    public float AttackTime;
+    public float WindDownTime;
+}
